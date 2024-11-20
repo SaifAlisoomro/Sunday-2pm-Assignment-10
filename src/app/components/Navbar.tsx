b4c5af64e1aaac1cdb9afad03edcfb5c2228da31
SaifAlisoomro <hk4948171@gmail.com>
2024-11-20 17:02:21 +0500

    assignment 10 update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className='flex justify-around items-center font-sans font-bold  flex-wrap my-5 py-5 sm:'>
       <div className='flex items-center gap-x-3 text-2xl py-5'>
 <Link href={"#"}> <FaCartShopping className='text-4xl text-gray-600 ' /></Link>
-<h1 className='text-[#044e83]'>Dream Mart</h1>
+<h1 className='text-[#044e83]'>DREAM MART</h1>
       </div>
       <ul className='flex justify-around gap-x-5 text-gray-500 '>
         <Link href={"#"}> Home</Link>
